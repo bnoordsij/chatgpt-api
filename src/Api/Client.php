26fae22a4fb1de7d02641fbf1f308d9ece235c86
Bart <bart.noordsij@gmail.com>
2023-05-27 15:26:02 +0200

    moved back folders

---
+++ src/Api/Client.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bnoordsij\ChatgptApi\Api;
+
+class Client
+{
+    public static function get()
+    {
+
+        return [
+            'data' => "Successful API call",
+        ];
+    }
+}
